27c269212f08ee138b05ad77ba075191f53c5565
Simon Sure <info@simonsure.com>
2023-06-16 18:47:44 +0200

    adding basic backend layout

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello from the backend api data!'}
+    return jsonify(data)
+
+
+@app.route('/api/backend', methods=['GET'])
+def get_index():
+    data = {'message': 'Hello from the backend!'}
+    return jsonify(data)
 
 
 @app.route('/')
