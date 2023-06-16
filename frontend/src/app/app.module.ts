621da654f4372a0019b05c279bd788da7e72ca82
Niklas <Gykonik@gmail.com>
2023-06-16 19:11:27 +0200

    Created Rough Layout Sketch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {GoogleMapsModule} from '@angular/google-maps';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MapComponentComponent } from './map-component/map-component.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MapComponentComponent} from './map-component/map-component.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 import {ButtonModule} from "primeng/button";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RippleModule} from "primeng/ripple";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponentComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    GoogleMapsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     ButtonModule,
@@ -25,4 +29,5 @@ import {RippleModule} from "primeng/ripple";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
