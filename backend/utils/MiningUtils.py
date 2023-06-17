ad7acc1c903757ddccffd24bfb2b790bed703885
Batıkan Ormancı <bati.ormanc@tum.de>
2023-06-17 18:07:44 +0200

    Add simple gmaps response printing for better debugging

--- backend/utils/MiningUtils.py
+++ backend/utils/MiningUtils.py
@@ -1,14 +1,33 @@
 
 from GmapsUtils import calculate_route_gmaps
+import pprint
+import sys
+import os
 def predictAndDescribeClimate(distance, duration):
     print(f"{distance=} {duration=}")
+
 
 
 
 if __name__ == "__main__":
     decoded_points, gmaps_response, distance, duration = calculate_route_gmaps("Berlin%20Hauptbahnhof", "Karlsruhe%20Hauptbahnhof", 
                           "public")
+    
+
+    # Open the file in write mode
+    file_path = './documents/gmaps_response.txt' 
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w') as file:
+        # Redirect the output to the file
+        sys.stdout = file
+
+        # Use pprint.pprint() to print the string_data
+        pprint.pprint(gmaps_response)
+
+    # Reset the stdout to the default value (console)
+    sys.stdout = sys.__stdout__
+
+
     if decoded_points: # meaning gmaps did return some OK response
         predictAndDescribeClimate(distance, duration)
-
 
