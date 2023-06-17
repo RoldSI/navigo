cfbe4105d528eb401d46b4d3d7302311a34300f7
Simon Sure <info@simonsure.com>
2023-06-17 10:01:31 +0200

    Add authentication function

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,19 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 favorites = []
 
+
+def authenticate_user(bearer_token):
+    if bearer_token is None:
+        return None
+    try:
+        decoded_token = auth.verify_id_token(bearer_token)
+        uid = decoded_token['uid']
+        return uid
+    except Exception as e:
+        # Authentication failed
+        return None
+
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {'message': 'Hello from the backend api data!'}
@@ -19,6 +32,7 @@ def get_index():
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/api/favorites/add', methods=['POST'])
 def add_favorite():
     data = request.get_json()
@@ -28,6 +42,7 @@ def add_favorite():
         return jsonify({'message': 'Favorite added successfully'})
     else:
         return jsonify({'message': 'Invalid request'})
+
 
 @app.route('/api/favorites/remove', methods=['POST'])
 def remove_favorite():
@@ -42,5 +57,19 @@ def remove_favorite():
     else:
         return jsonify({'message': 'Invalid request'})
 
+<<<<<<< Updated upstream
+=======
+
+@app.route('/authenticate', methods=['GET'])
+def authenticate_user():
+    bearer_token = request.headers.get('Authorization')
+    uid = authenticate_user(bearer_token)
+    if uid is None:
+        return 'authentication failed'
+    else:
+        return 'authentication successful'
+
+
+>>>>>>> Stashed changes
 if __name__ == '__main__':
     app.run()
