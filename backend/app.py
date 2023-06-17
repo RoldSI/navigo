6b340f9dedb69332f11c1a60b27161b29a3e60d9
Simon Sure <info@simonsure.com>
2023-06-17 12:32:35 +0200

    Add database integration

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
 from firebase_admin import auth
 from flask import Flask, jsonify, request
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, firestore
 import openai
 from utils import GmapsUtils
 from dotenv import dotenv_values
 
 env_vars = dotenv_values("../.env")
 OPENAI_API_KEY = env_vars["OPENAI_API_KEY"]
-
 openai.api_key = OPENAI_API_KEY
+
 cred = credentials.Certificate('firebaseCredentials.json')
-firebase_admin.initialize_app(cred)
+firebaseApp = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 favorites = []
@@ -31,58 +32,118 @@ def authenticate_user(bearer_token):
 
 @app.route('/api/favorites', methods=['POST'])
 def add_favorite():
-    favorite = request.json['input']
-    if favorite in favorites:
-        response = {'message': 'Favorite already contained'}
+    bearer_token = request.headers.get('Authorization')
+    uid = authenticate_user(bearer_token)
+    # uid = 'hoi'
+    if uid is None:
+        return 'authentication failed'
+    user_favorites_doc_ref = db.collection("favorites").document(uid)
+    user_favorites_list = user_favorites_doc_ref.get().to_dict()
+    if user_favorites_list is None:
+        user_favorites_list = []
     else:
-        favorites.append(favorite)
-        response = {'message': 'Favorite added successfully'}
-    return jsonify(response)
+        user_favorites_list = user_favorites_list["favorites"]
+    print(user_favorites_list)
+    new_favorites = request.json['input']
+    for favorite in new_favorites:
+        if favorite not in user_favorites_list:
+            user_favorites_list.append(favorite)
+            print(f"{favorite} added to favorites")
+        else:
+            print(f"{favorite} already in favorites")
+    user_favorites_doc_ref.set({"favorites": user_favorites_list})
+    return 'Provided favorites added successfully'
 
 
 @app.route('/api/favorites', methods=['DELETE'])
 def remove_favorite():
-    favorite = request.json['input']
-    if favorite in favorites:
-        favorites.remove(favorite)
-        response = {'message': 'Favorite removed successfully'}
+    bearer_token = request.headers.get('Authorization')
+    uid = authenticate_user(bearer_token)
+    # uid = 'hoi'
+    if uid is None:
+        return 'authentication failed'
+    user_favorites_doc_ref = db.collection("favorites").document(uid)
+    user_favorites_list = user_favorites_doc_ref.get().to_dict()
+    if user_favorites_list is None:
+        user_favorites_list = []
     else:
-        response = {'message': 'Favorite not found'}
-    return jsonify(response)
+        user_favorites_list = user_favorites_list["favorites"]
+    print(user_favorites_list)
+    remove_favorites = request.json['input']
+    for favorite in remove_favorites:
+        if favorite in user_favorites_list:
+            user_favorites_list.remove(favorite)
+            print(f"{favorite} removed from favorites")
+        else:
+            print(f"{favorite} not in favorites")
+    user_favorites_doc_ref.set({"favorites": user_favorites_list})
+    return 'Provided favorites removed successfully'
 
 
 @app.route('/api/favorites', methods=['GET'])
 def get_favorites():
-    return 'this should return favorites'
+    bearer_token = request.headers.get('Authorization')
+    uid = authenticate_user(bearer_token)
+    # uid = 'hoi'
+    if uid is None:
+        return 'authentication failed'
+    user_favorites_doc_ref = db.collection("favorites").document(uid)
+    user_favorites_list = user_favorites_doc_ref.get().to_dict()
+    if user_favorites_list is None:
+        user_favorites_list = []
+    else:
+        user_favorites_list = user_favorites_list["favorites"]
+    print(user_favorites_list)
+    return jsonify({"favorites": user_favorites_list})
 
 
 @app.route('/api/suggestions', methods=['GET'])
 def generate_suggestion():
-  location = request.json['input'] #get data from frontend
-  message = [ {"role": "user", "content": f"What are some things to do in {location}? Your answer should not exceed 25 words."} ]
-  chat = openai.ChatCompletion.create(
-      model="gpt-4", messages=message
-  )
-  reply = chat.choices[0].message.content
-  return reply
+    location = request.json['input']  # get data from frontend
+    message = [{"role": "user",
+                "content": f"What are some things to do in {location}? Your answer should not exceed 25 words."}]
+    chat = openai.ChatCompletion.create(
+        model="gpt-4", messages=message
+    )
+    reply = chat.choices[0].message.content
+    return reply
 
 
 @app.route('/api/routes', methods=['GET'])
 def routing():
-    #request_data = request.args
-    #from_param = request_data.get('from')
-    #to_param = request_data.get('to')
+    # request_data = request.args
+    # from_param = request_data.get('from')
+    # to_param = request_data.get('to')
     request_data = {
         'from': 'Berlin',
         'to': 'Munich',
     }
-    (decoded_points, gm, distance, duration) = GmapsUtils.calculate_route_gmaps(request_data['from'], request_data['to'], 'walking')
+    (w_dp, w_gm, w_di, w_du) = GmapsUtils.calculate_route_gmaps(
+        request_data['from'],
+        request_data['to'],
+        'walking'
+    )
+    (b_dp, b_gm, b_di, b_du) = GmapsUtils.calculate_route_gmaps(
+        request_data['from'],
+        request_data['to'],
+        'biking'
+    )
+    (d_dp, d_gm, d_di, d_du) = GmapsUtils.calculate_route_gmaps(
+        request_data['from'],
+        request_data['to'],
+        'driving'
+    )
+    (p_dp, p_gm, p_di, p_du) = GmapsUtils.calculate_route_gmaps(
+        request_data['from'],
+        request_data['to'],
+        'public'
+    )
     response_data = {
         'walking': {
-            'distance': distance,  # meters
-            'duration': duration,  # minutes
-            'decoded_points': decoded_points,  # array of waypoints
-            'efficiency': 34  # (1)/((time * factor) * (co2 * factor))
+            'distance': w_di,  # meters
+            'duration': w_du,  # minutes
+            'decoded_points': w_dp,  # array of waypoints
+            'efficiency': 100  # (1)/((time * factor) * (co2 * factor))
         },
         'biking': {
             'distance': 30,  # meters
